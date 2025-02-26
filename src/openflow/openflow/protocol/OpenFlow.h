4f8faa89477912d85a390872b044583bb1df39f4
Zoltan Bojthe <zbojthe@omnetpp.org>
2025-02-26 12:37:04 +0100

    inet4-porting: OpenFlow.h: added INET version test

--- src/openflow/openflow/protocol/OpenFlow.h
+++ src/openflow/openflow/protocol/OpenFlow.h
@@ -1,7 +1,14 @@
+
+#include <inet/common/INETDefs.h>
+
 #define OFP_100 0x01
 #define OFP_135 0x04 //currently not supported.
 #define OFP_141 0x05 //currently not supported.
 #define OFP_151 0x06 //currently not supported.
+
+#if INET_VERSION < 0x0403 || INET_VERSION == 0x0403 && INET_PATCH_LEVEL < 0x02
+#error OpenFlow: Incompatible INET version. At least INET version v4.3.2 required.
+#endif
 
 #ifndef OFP_VERSION_IN_USE
     #define OFP_VERSION_IN_USE OFP_100
